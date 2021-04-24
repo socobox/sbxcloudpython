b2ec6d4c9d6fe8ce925ab5539c3b5aaf7df729f0
Luis Guzman <lgguzman890414@gmail.com>
2021-04-24 09:50:22 -0500

    add support event

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sbxpy",
-    version="0.1.0",
+    version="0.1.1",
     author="Luis Guzmán",
     author_email="lgguzman890414@gmail.com",
     description="This is the module  create all request used to communicate with SbxCloud",
